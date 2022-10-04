076268e5b874cedcefa4306ec6d10743c41f54c8
dominik-kiss <dominik.kiss997@gmail.com>
2022-10-04 11:29:43 +0200

    Re-structure code to be more modular, add Mustache template, process form input
    Re-structure code to make it simpler and not pollute global namespace.
    
    Use Mustache template for the card creation.
    
    Add functionality to process form input without server.

--- script.js
+++ script.js
@@ -1,66 +1,104 @@
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-// }
 
-(function() {
+
+let myLibrary = (function() {
+
+    let myLibrary = [];
+
+    function Book(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.readText = this.read ? "Read":"Not read";
+        this.readStyle = this.read ? "read":"not-read";
+    }
+
+    function addBookToLibrary(title, author, pages, read) {
+        myLibrary.push(new Book(title, author, pages, read));
+    }
+
+
+    addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+    addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 250, true);
+    addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 275, true);
+    addBookToLibrary("Beloved", "Toni Morrison", 150, false);
     
-    let myLibrary = {
-        myLibrary: [
-            {
-                "title": "The Hobbit",
-                "author": "J.R.R. Tolkien",
-                "pages": 295,
-                "read": true
-            },
-            {
-                "title": "The Great Gatsby",
-                "author": "F. Scott Fitzgerald",
-                "pages": 250,
-                "read": false
-            },
-            {
-                "title": "To Kill a Mockingbird",
-                "author": "Harper Lee",
-                "pages": 275,
-                "read": false
-            },
-            {
-                "title": "Beloved",
-                "author": "Toni Morrison",
-                "pages": 150,
-                "read": true
-            },
-            {
-                "title": "Example",
-                "author": "Authorexample",
-                "pages": 84,
-                "read": true
-            }
-        ],
-        init: function() {
-            this.cacheDOM();
-            this.render();
-        },
-        cacheDOM: function() {
-            this.cardsContainer = document.querySelector("#cards-container");
-            this.template = this.cardsContainer.querySelector("#card-template").innerHTML;
-        },
-        render: function() {
-            this.myLibrary.forEach((book) => {
-                console.log(book);
-                this.cardsContainer.innerHTML = Mustache.render(this.template, book);
-            });
-        }
-    };
 
-    myLibrary.init();
+    // Cache DOM
+    let cardsContainer = document.querySelector("#cards-container");
+    let newBookButton = document.getElementById("add-button");
+    let form = document.getElementById("add-form");
+    let submitButton = form.querySelector("#submit-button");
+    let cancelButtons;
+    let titles;
+    let authors;
+    let pages;
+    let reads;
+
+    
+    render();
+
+    // Bind listeners that will not change
+
+    newBookButton.addEventListener("click", toggleForm);
+    formClose.addEventListener("click", toggleForm);
+    submitButton.addEventListener("click", addCard);
+
+
+
+    function render() {
+
+        cardsContainer.innerHTML = Mustache.render('\
+        {{#myLibrary}}\
+        <div class="card">\
+            <i class="material-icons close-button">close</i>\
+            <p class="title">{{title}}</p>\
+            <p class="author">{{author}}</p>\
+            <p class="pages">{{pages}}</p>\
+            <button class="{{readStyle}}">{{readText}}</button>\
+        </div>\
+        {{/myLibrary}}\
+        ', { myLibrary: myLibrary });
+        bindListeners();
+    }
+
+    function bindListeners() {
+
+        // Cache newly created DOM elements
+        cancelButtons = cardsContainer.querySelectorAll(".close-button");
+        titles = cardsContainer.querySelectorAll(".title");
+        authors = cardsContainer.querySelectorAll(".author");
+        pages = cardsContainer.querySelectorAll(".pages");
+        reads = cardsContainer.querySelectorAll(".read, .not-read");
+        
+        cancelButtons.forEach(button => button.addEventListener("click", removeCard));    
+    }
+
+    function toggleForm() {
+        clearForm()
+        form.classList.toggle("hidden");
+    }
+
+    function addCard() {
+        addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
+        toggleForm();
+        render();
+    }
+
+    function removeCard() {
+        let index = [...cardsContainer.children].indexOf(this.parentElement);
+        myLibrary.splice(index, 1);
+        render();
+    }
+
+    function clearForm() {
+        titleInput.value = "";
+        authorInput.value = "";
+        pagesInput.value = "";
+        readInput.checked = false;
+    }
+
 })();
-
-
-
 
 // let cancelButtons = document.querySelectorAll(".close-button").forEach(button => button.addEventListener("click", removeBook));
 // document.querySelectorAll(".read, .not-read").forEach(button => button.addEventListener("click", switchRead));
